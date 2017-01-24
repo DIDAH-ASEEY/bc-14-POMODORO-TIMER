d1ea8f446a0cf305a94d9cea1ae1ec41b3744c7a
DIDAH-ASEEY <aseeyd@gmail.com>
2017-01-24 14:44:33 +0300

    created pomodoro database and table

--- database.py
+++ database.py
@@ -6,5 +6,23 @@ def create_table():
     conn = sqlite3.connect('pomodoro_timer.db')
     c = conn.cursor()
 
+    print "succesfully connected"
+
+    conn.execute('''CREATE TABLE POMODORO_TIMER
+       (ID INT PRIMARY KEY     NOT NULL,
+       TASK_TITTLE     TEXT    NOT NULL,
+       TIMESTAMP       TEXT    NOT NULL,
+       CYCLES          INT,
+       SHORT_BREAK     TEXT,
+       LONG_BREAK      TEXT,
+       TOTAL_DURATION  TEXT,
+       SOUND           INT);''')
+
+    print "table created successfully"
+
+create_table()
+
+    
+
 
 
